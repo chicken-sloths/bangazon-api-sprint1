9e7e74062b6aacff786282829927834b860d129c
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-20 16:06:43 -0500

    employee-training program join table geenrated successfully, order outlined in index

--- data/fakers/employees/employeeTrainingProgramFaker.js
+++ data/fakers/employees/employeeTrainingProgramFaker.js
@@ -2,5 +2,15 @@
 
 const faker = require("faker");
 
-module.exports.generateEmployeeTrainingProgramRelationships = () => {
+module.exports.generateEmployeeTrainingProgramRelationships = (employeeCount, trainingProgramCount) => {
+  let employeeTrainingProgramRelationships = [];
+  for (let i=0; i<employeeCount; i++) {
+    let employee_id = Math.floor(Math.random()*employeeCount) + 1;
+    let training_program_id = Math.floor(Math.random()*trainingProgramCount) + 1;
+    employeeTrainingProgramRelationships.push({
+      employee_id,
+      training_program_id
+    });
+  }
+  return employeeTrainingProgramRelationships;
 };
--- data/fakers/employees/index.js
+++ data/fakers/employees/index.js
@@ -5,7 +5,22 @@ const { generateDepartments } = require("./departmentFaker");
 const { generateEmployeeComputerRelationships } = require("./employeeComputerFaker");
 const { generateEmployees } = require("./employeeFaker");
 const { generateEmployeeTrainingProgramRelationships } = require("./employeeTrainingProgramFaker");
-const { generateTrainingPrograms } = require("./trainingProgramFaker");
+const { generateTrainingPrograms } = require("./trainingProgramFaker")
+
+// SAMPLE CALLS
+
+// ASSUMPTION: 
+//  10 departments
+//  25 employees
+//  25 computers
+//  10 training programs
+
+// generateDepartments(10);
+// generateEmployees(25, 10);
+// generateComputers(25);
+// generateEmployeeComputerRelationships(25);
+// generateTrainingPrograms(10);
+// generateEmployeeTrainingProgramRelationships(25,10);
 
 module.exports = {
   generateComputers,
