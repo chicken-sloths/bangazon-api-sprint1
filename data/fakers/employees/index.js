9bce1d0d549a0766d007259c969b72238d67ad10
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-20 15:26:24 -0500

    all functions requried in index

--- data/fakers/employees/index.js
+++ data/fakers/employees/index.js
@@ -1,12 +1,17 @@
 "use strict";
 
 const { generateEmployees } = require("./employeeFaker");
+const { generateDepartments } = require("./departmentFaker");
+const { generateEmployeeComputerRelationships } = require("./employeeComputerFaker");
+const { generateEmployeeTrainingProgramRelationships } = require("./employeeTrainingProgramFaker");
+const { generateEmployees } = require("./employeeFaker");
+const { generateTrainingPrograms } = require("./trainingProgramFaker")
 
 module.exports = {
-    generateComputers,
-    generateDepartments,
-    generateEmployeeComputerRelationships,
-    generateEmployeeTrainingProgramRelationships,
-    generateEmployees,
-    generateTrainingPrograms
+  generateComputers,
+  generateDepartments,
+  generateEmployeeComputerRelationships,
+  generateEmployeeTrainingProgramRelationships,
+  generateEmployees,
+  generateTrainingPrograms
 };
