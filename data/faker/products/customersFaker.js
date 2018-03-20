8652be347f4e3fec51c611d43dfe6d9c1b90f47f
Tim A. Conner <timaconner1@gmail.com>
2018-03-20 16:00:59 -0500

    Complete generateCustomers().

--- data/faker/products/customersFaker.js
+++ data/faker/products/customersFaker.js
@@ -1,8 +1,26 @@
 'use strict';
 const faker = require('faker');
 
-const generateCustomers = () => {
-  return [{}];
+const generateCustomers = (amount = 25) => {
+  const customers = [];
+
+  for (let i = 0; i < amount; i++) {
+    const customer = {
+      customer_id: i,
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      account_creation_date: faker.date.recent(),
+      street_address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      postal_code: faker.address.zipCode(),
+      phone_number: faker.phone.phoneNumber()
+    };
+
+    customers.push(customer);
+  }
+
+  return customers;
 };
 
 module.exports = {
