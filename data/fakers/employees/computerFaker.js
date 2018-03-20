e2854808d18ec14553d43c9c8f99caa428f1d566
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-20 15:49:02 -0500

    mid computer faker

--- data/fakers/employees/computerFaker.js
+++ data/fakers/employees/computerFaker.js
@@ -2,5 +2,20 @@
 
 const faker = require("faker");
 
-module.exports.generateComputers = () => {
+module.exports.generateComputers = (limit) => {
+  let computers = [];
+  for (let i=0; i<limit; i++) {
+    let mac_address = faker.internet.mac();
+    let purchase_date = faker.date.past(5, new Date());
+    // TODO: decommission date
+    // ASSUMPTION: there are as many computers as employees
+    let employee_id = Math.floor(Math.random() * limit) + 1;
+    computers.push({
+      mac_address,
+      employee_id,
+      purchase_date
+      // TODO: decommission date
+    });
+  }
+  return computers;
 };
