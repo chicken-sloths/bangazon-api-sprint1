730367700d46c3f9bde916f211dbfd9cdf0c5d7b
Tim A. Conner <timaconner1@gmail.com>
2018-03-20 16:04:02 -0500

    Complete generateOrders().

--- data/faker/products/ordersFaker.js
+++ data/faker/products/ordersFaker.js
@@ -1,8 +1,20 @@
 'use strict';
 const faker = require('faker');
 
-const generateOrders = () => {
-  return [{}];
+const generateOrders = (amount = 25) => {
+  const orders = [];
+
+  for (let i = 0; i < amount; i++) {
+    const order = {
+      order_id: i,
+      customer_id: i,
+      payment_type_id: i
+    };
+
+    orders.push(order);
+  }
+
+  return orders;
 };
 
 module.exports = {
