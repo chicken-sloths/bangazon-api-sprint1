91608a1d1c3235c9af733bc583c1aa29db358b4a
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-20 15:59:42 -0500

    employee computer join complete, training program generator complete

--- data/fakers/employees/employeeComputerFaker.js
+++ data/fakers/employees/employeeComputerFaker.js
@@ -2,5 +2,20 @@
 
 const faker = require("faker");
 
-module.exports.generateEmployeeComputerRelationships = () => {
+// ASSUMPTION: limit is equal to the number of computers AND the number of employees
+module.exports.generateEmployeeComputerRelationships = (limit) => {
+  let employeeComputerRelationships = [];
+  for (let i=0; i<limit; i++) {
+    let computer_id = Math.floor(Math.random()*limit)+1;
+    let employee_id = Math.floor(Math.random()*limit)+1;
+    let start_date = faker.date.past(5, new Date());
+    let end_date = faker.date.between(start_date, new Date());
+    employeeComputerRelationships.push({
+      computer_id,
+      employee_id,
+      start_date,
+      end_date
+    });
+  }
+  return employeeComputerRelationships;
 };
--- data/fakers/employees/trainingProgramFaker.js
+++ data/fakers/employees/trainingProgramFaker.js
@@ -2,5 +2,20 @@
 
 const faker = require("faker");
 
-module.exports.generateTrainingPrograms = () => {
+module.exports.generateTrainingPrograms = (limit) => {
+  let trainingPrograms = [];
+  for (let i=0; i<limit; i++) {
+    let name = faker.fake("{{name.jobArea}} 101");
+    let start_date = faker.date.past(5, new Date());
+    let end_date = faker.date.between(start_date, new Date());
+    // max_capacity = random number between 10 and 20
+    let max_capacity = Math.floor(Math.random() * 10) + 10;
+    trainingPrograms.push({
+      name,
+      start_date,
+      end_date,
+      max_capacity
+    });
+  }
+  return trainingPrograms;
 };
