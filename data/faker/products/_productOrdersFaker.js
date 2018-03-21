e3370d7d895949f931e7b30ad29c4d0e69fd4f27
Tim A. Conner <timaconner1@gmail.com>
2018-03-21 13:57:00 -0500

    Modify the array outputted by generateProductOrders() to have unique product_order_ids.

--- data/faker/products/_productOrdersFaker.js
+++ data/faker/products/_productOrdersFaker.js
@@ -4,8 +4,9 @@ const randomInt = require('../randomInt');
 
 const _generateProductOrders = ({ customerAmount, maxProductsOnOrder, productAmount }, orders) => {
   let productOrders = [];
+  let curProductOrderId = 0;
 
-  // Create a product for each customer
+  // Find an order for each customer
   for (let i = 0; i < customerAmount; i++) {
 
     // Check if a customer has an order
@@ -16,13 +17,17 @@ const _generateProductOrders = ({ customerAmount, maxProductsOnOrder, productAmo
     if (customerOrder !== null) {
       const randomAmountOfProducts = randomInt(maxProductsOnOrder) + 1;
 
+      // Randomly selects randomAmountOfProducts of products
       for (let j = 0; j < randomAmountOfProducts; j++) {
         const randomProductId = randomInt(productAmount);
         const productOrder = {
-          product_order_id: i,
+          product_order_id: curProductOrderId,
           product_id: randomProductId,
           order_id: customerOrder.order_id
         };
+
+        // Increment the current product order id for the next product
+        curProductOrderId++;
 
         productOrders.push(productOrder);
       }
