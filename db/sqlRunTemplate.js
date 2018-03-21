e2594dfed0d3edc009d1cc18b9c01949148487db
Tim A. Conner <timaconner1@gmail.com>
2018-03-21 15:46:24 -0500

    Add genereateSqlTable function that  will drop if need be, create, and insert data into the specified table.

---
+++ db/sqlRunTemplate.js
@@ -0,0 +1,65 @@
+'use strict';
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("api-sprint.sqlite");
+
+/*
+EXAMPLE FUNCTION CALL
+
+generateSqlTable(
+  {
+    tableName: `commputers`,
+    columns: 
+      `computer_id INTEGER PRIMARY KEY,
+      purchase_date TEXT,
+      decommission_date TEXT,
+      a_number INTEGER,
+      thirty_seven INTEGER`,
+    dataToIterateOver: computers,
+    valuesToInsert: [
+      null,
+      `purchaseDate`, 
+      `decommissionDate`, 
+      `aNumber`, 
+      37]
+  }
+);
+*/
+
+const generateSqlTable = ({ tableName, columns, dataToIterateOver, valuesToInsert }) => {
+  db.serialize(() => {
+    db.run(`DROP TABLE IF EXISTS ${tableName}`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (${columns})`);
+
+    dataToIterateOver.forEach(object => {
+      let values = "";
+
+      // Iterate over the values to insert and turn them into sql code
+      for (let i = 0; i < valuesToInsert.length; i++) {
+        const value = valuesToInsert[i];
+
+        // Add a comma to the string if it is not the last value in the column
+        const addComma = i < valuesToInsert.length - 1 ? "," : "";
+
+        // If the valueToInsert itself is a number
+        if (Number.isInteger(value)) {
+          values += `${value}${addComma}`;
+        }
+
+        // If the value on the object is an integer, add it.  
+        else if (Number.isInteger(object[value])) {
+          values += `${object[value]}${addComma}`;
+        }
+
+        // If it is a string, add quotes around it.
+        else {
+          values += `${value === null ? null : "'" + object[value] + "'"}${addComma}`;
+        }
+      }
+
+      db.run(`INSERT INTO ${tableName} VALUES (${values})`);
+    });
+  });
+};
+
+module.exports = { generateSqlTable };
