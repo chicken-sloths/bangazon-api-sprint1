60547cdf313e730c44a32a325204abf3a972050c
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-21 15:25:20 -0500

    commented out routes in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const routes = require('./routes/index.js'); 
+// const routes = require('./routes/index.js'); 
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
