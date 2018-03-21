3e155910f85e8c1d5fd26e166be135e0dce97bc4
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-21 15:02:21 -0500

    all data is generated, added all json files, can tolerate a missing json folder

--- data/build-data.js
+++ data/build-data.js
@@ -1,51 +1,86 @@
+"use strict";
+
 const { generateProductData } = require("./faker/products/index");
 const { generateEmployeeData } = require("./faker/employees/index");
 const {
   createWriteStream,
-  existsSync,
-  mkdirSync
+  exists,
+  mkdir
 } = require('fs');
 const path = require("path");
 
 let jsonDir = __dirname+"/json";
-if (existsSync(path.dirname(jsonDir))) {
-  mkdirSync(jsonDir);
-}
 
-// customer half
-let {
-  customers,
-  productTypes,
-  products,
-  paymentOptions,
-  orders,
-  productOrders
-} = generateProductData();
+// checks if data/json/ folder exists
+exists(jsonDir, exists => {
+  if (exists) {
+    writeJson(jsonDir);
+  } else {
+    // makes it if it doesn't
+    mkdir(jsonDir, err => {
+      if (err) {
+        console.log("mkdir error:",err);
+      } else {
+        writeJson(jsonDir);
+      }
+    });
+  }
+});
 
-let customerStream = createWriteStream(jsonDir+"/customers.json");
-customerStream.write(JSON.stringify(customers));
+const writeJson = (jsonDir) => {
+  // customer half
+  let {
+    customers,
+    productTypes,
+    products,
+    paymentOptions,
+    orders,
+    productOrders
+  } = generateProductData();
 
-let optionStream = createWriteStream(jsonDir+"/products.json");
-optionStream.write(JSON.stringify(products));
+  let customerStream = createWriteStream(jsonDir+"/customers.json");
+  customerStream.write(JSON.stringify(customers));
 
-let productTypeStream = createWriteStream(jsonDir+"/productTypes.json");
-productTypeStream.write(JSON.stringify(productTypes));
+  let optionStream = createWriteStream(jsonDir+"/products.json");
+  optionStream.write(JSON.stringify(products));
 
-let paymentOptionStream = createWriteStream(jsonDir+"/paymentOptions.json");
-paymentOptionStream.write(JSON.stringify(paymentOptions));
+  let productTypeStream = createWriteStream(jsonDir+"/productTypes.json");
+  productTypeStream.write(JSON.stringify(productTypes));
 
-let orderStream = createWriteStream(jsonDir+"/orders.json");
-orderStream.write(JSON.stringify(orders));
+  let paymentOptionStream = createWriteStream(jsonDir+"/paymentOptions.json");
+  paymentOptionStream.write(JSON.stringify(paymentOptions));
 
-let productOrderStream = createWriteStream(jsonDir+"/productOrders.json");
-productOrderStream.write(JSON.stringify(productOrders));
+  let orderStream = createWriteStream(jsonDir+"/orders.json");
+  orderStream.write(JSON.stringify(orders));
 
-// employee half
-let {
-  departments,
-  employees,
-  computers,
-  employeeComputerRelationships,
-  trainingPrograms,
-  employeeTrainingProgramRelationships
-} = generateEmployeeData();
+  let productOrderStream = createWriteStream(jsonDir+"/productOrders.json");
+  productOrderStream.write(JSON.stringify(productOrders));
+
+  // employee half
+  let {
+    departments,
+    employees,
+    computers,
+    employeeComputerRelationships,
+    trainingPrograms,
+    employeeTrainingProgramRelationships
+  } = generateEmployeeData();
+  
+  let departmentStream = createWriteStream(jsonDir+"/departments.json");
+  departmentStream.write(JSON.stringify(departments));
+  
+  let employeeStream = createWriteStream(jsonDir+"/employees.json");
+  employeeStream.write(JSON.stringify(employees));
+  
+  let computerStream = createWriteStream(jsonDir+"/computers.json");
+  computerStream.write(JSON.stringify(computers));
+  
+  let employeeComputerStream = createWriteStream(jsonDir+"/employeeComputers.json");
+  employeeComputerStream.write(JSON.stringify(employeeComputerRelationships));
+  
+  let trainingProgramStream = createWriteStream(jsonDir+"/trainingPrograms.json");
+  trainingProgramStream.write(JSON.stringify(trainingPrograms));
+  
+  let employeeTrainingProgramStream = createWriteStream(jsonDir+"/employeeTrainingPrograms.json");
+  employeeTrainingProgramStream.write(JSON.stringify(employeeTrainingProgramRelationships));
+};
