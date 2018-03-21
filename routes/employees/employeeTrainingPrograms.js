bb7952a2f05bd3ba5a3e6804ee6101b4aabc03ec
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-21 15:00:52 -0500

    Updates filepaths in routes/employees/ to absolute filepaths

--- routes/employees/employeeTrainingPrograms.js
+++ routes/employees/employeeTrainingPrograms.js
@@ -1,9 +1,11 @@
 "use strict";
 
+const appRoot = process.cwd();
+
 const { Router } = require('express');
 const employeeTrainingProgramsRouter = Router();
 
-const controller = require('../controllers/employees/employeeTrainingProgramsCtrl');
+const controller = require(appRoot + '/controllers/employees/employeeTrainingProgramsCtrl');
 
 //TODO: routes & their corresponding controllers are enumerated here
 employeeTrainingProgramsRouter.get('/', controller);
--- routes/employees/employees.js
+++ routes/employees/employees.js
@@ -1,9 +1,11 @@
 "use strict";
 
+const appRoot = process.cwd();
+
 const { Router } = require('express');
 const employeesRouter = Router();
 
-const controller = require('../controllers/employees/employeesCtrl');
+const controller = require(appRoot + '/controllers/employees/employeesCtrl');
 
 //TODO: routes & their corresponding controllers are enumerated here
 employeesRouter.get('/', controller);
