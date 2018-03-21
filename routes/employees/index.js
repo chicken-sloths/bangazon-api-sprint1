31e0d86447ac998ac98e1c6c7fe4aa3c34a305ff
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-21 14:21:06 -0500

    Fixes incorrect naming; adds an index for employees/ & for products/ to funnel better

---
+++ routes/employees/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const { Router } = require('express');
+const employeesIndexRouter = Router();
+
+employeesIndexRouter.use(require('./departments'));
+employeesIndexRouter.use(require('./employees'));
+employeesIndexRouter.use(require('./employeeComputers'));
+employeesIndexRouter.use(require('./computers'));
+employeesIndexRouter.use(require('./trainingPrograms'));
+employeesIndexRouter.use(require('./employeeTrainingPrograms'));
+
+module.exports = employeesIndexRouter;
--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,9 @@ const { Router } = require('express');
 const router = Router();
 
 // Pulling in all routes for employees/
-app.use(require('./employees/departments'));
-app.use(require('./employees/employees'));
-app.use(require('./employees/employeeComputers'));
-app.use(require('./employees/computers'));
-app.use(require('./employees/trainingPrograms'));
-app.use(require('./employees/employeeTrainingPrograms'));
+router.use(require('./employees/index'));
 
 // Pulling in all routes for products/
-app.use(require('./products/productTypes'));
-app.use(require('./products/products'));
-app.use(require('./products/productOrders'));
-app.use(require('./products/orders'));
-app.use(require('./products/customers'));
-app.use(require('./products/paymentOptions'));
+router.use(require('./products/index'));
 
 module.exports = router;
