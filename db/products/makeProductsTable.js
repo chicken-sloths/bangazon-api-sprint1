3acf0487fcb2037827fdd9070562bbd5f6d7072c
Tim A. Conner <timaconner1@gmail.com>
2018-03-22 09:35:29 -0500

    Complete: Create Product Table.

--- db/products/makeProductsTable.js
+++ db/products/makeProductsTable.js
@@ -2,7 +2,33 @@
 
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("api-sprint.sqlite");
+const { readFileSync } = require("fs");
+const { generateSqlTable } = require('../sqlRunTemplate');
+
+const products = JSON.parse(readFileSync("../data/json/products.json"));
 
 module.exports = () => {
-  // function that makes product tables
+  generateSqlTable(
+    {
+      tableName: `Products`,
+      columns: 
+        `product_id INTEGER PRIMARY KEY,
+        price TEXT,
+        title TEXT,
+        description TEXT,
+        product_type_id INTEGER,
+        creator_id INTEGER,
+        FOREIGN KEY (product_type_id) REFERENCES Product_Type(product_type_id),
+        FOREIGN KEY (creator_id) REFERENCES Customers(customer_id)`,
+      dataToIterateOver: products,
+      valuesToInsert: [
+        `product_id`,
+        `price`, 
+        `title`, 
+        `description`, 
+        `product_type_id`, 
+        `creator_id`
+      ]
+    }
+  );
 }
