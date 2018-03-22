4bd5286d7c2ecac5f970b9fcd67e987bfa7cd75d
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-22 14:34:05 -0500

    In employeesCtrl, defines handling EmployeesModel calls for GET fns

--- controllers/employees/employeesCtrl.js
+++ controllers/employees/employeesCtrl.js
@@ -2,7 +2,17 @@
 
 const appRoot = process.cwd();
 
-const model = require(appRoot + "/models/EmployeesModel");
+const { 
+  getAllEmployees, 
+  getSingleEmployee 
+} = require(appRoot + "/models/employees/EmployeesModel");
 
+module.exports.getAllEmployees = (req, res, next) =>
+  getAllEmployees()
+  .then(emps => res.status(200).json(emps))
+  .catch(err => next(err));
 
-module.exports = {  };
+module.exports.getSingleEmployee = (req, res, next) =>
+  getSingleEmployee(req.params.id)
+  .then(emp => res.status(200).json(emp))
+  .catch(err => next(err)); 
