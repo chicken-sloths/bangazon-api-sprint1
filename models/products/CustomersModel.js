a8d221563834f65052588c7fc868d11ff716d7e8
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-22 15:38:08 -0500

    Fixes #64 Sets up routes and end points for the Customers

--- models/products/CustomersModel.js
+++ models/products/CustomersModel.js
@@ -3,4 +3,50 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/api-sprint.sqlite');
 
-module.exports = {};
+const getAll = () =>{
+  return new Promise((resolve, reject)=>{
+    db.all(
+      `SELECT Customers.* FROM Customers`,
+      (error, customers)=>{
+        if (error) return reject(error);
+        resolve(customers);
+      }
+    );
+  });
+};
+
+const getOne = id =>{
+  return new Promise((resolve, reject)=>{
+    db.all(
+      `SELECT Customers.* FROM Customers
+      WHERE Customers.customer_id = ${id}`,
+      (error, customer)=>{
+        if (error) return reject(error);
+        resolve(customer);
+      }
+    );
+  });
+};
+
+// This function looks for all customers that do not have any PAST orders
+const getFrugalCustomers = () =>{
+  return new Promise((resolve, reject)=>{
+    db.all(`
+    SELECT Customers.* 
+    FROM Customers
+    LEFT JOIN Orders
+    ON Customers.customer_id = Orders.customer_id
+    WHERE Orders.customer_id IS NULL
+    `, 
+    (error, customers)=>{
+      if (error) return reject(error);
+        resolve(customers);
+    });
+  });
+};
+
+module.exports = { 
+  getAll,
+  getOne,
+  getFrugalCustomers
+};
--- routes/products/customers.js
+++ routes/products/customers.js
@@ -5,9 +5,11 @@ const appRoot = process.cwd();
 const { Router } = require('express');
 const customersRouter = Router();
 
-const controller = require(appRoot + '/controllers/products/customersCtrl');
+const { getAllCustomers, getOneCustomer } = require(appRoot + '/controllers/products/customersCtrl');
 
 //TODO: routes & their corresponding controllers are enumerated here
-customersRouter.get('/', controller);
+customersRouter.get('/customers/:id', getOneCustomer);
+customersRouter.get('/customers', getAllCustomers);
+
 
 module.exports = customersRouter;
