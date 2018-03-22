cb6abaf40ac339b6fd8ef2738637d41c6f4ddb99
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-03-22 09:31:13 -0500

    created customer table

--- db/products/makeCustomerTable.js
+++ db/products/makeCustomerTable.js
@@ -2,9 +2,36 @@
 
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("api-sprint.sqlite");
-
+const { generateSqlTable } = require("../sqlRunTemplate");
+const customers = require("../../data/json/customers.json");
 module.exports = () => {
-// function that creates customer table
+  generateSqlTable(
+    {
+      tableName: `Customers`,
+      columns:
+      `customer_id INTEGER PRIMARY KEY,
+      first_name TEXT,
+      last_name TEXT,
+      account_creation_date TEXT,
+      street_address TEXT,
+      city TEXT,
+      state TEXT,
+      postal_code TEXT,
+      phone_number TEXT`,
+      dataToIterateOver: customers,
+      valuesToInsert: [
+        `customer_id`,
+        `first_name`,
+        `last_name`,
+        `account_creation_date`,
+        `street_address`,
+        `city`,
+        `state`,
+        `postal_code`,
+        `phone_number`
+      ]
+    }
+  );
 }
 
 
