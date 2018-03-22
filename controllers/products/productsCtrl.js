1847ded90bdf9eb381df4e4036269422cd9a973f
Tim A. Conner <timaconner1@gmail.com>
2018-03-22 14:12:00 -0500

    Add getAllProducts and getSingleProduct controller.

--- controllers/products/productsCtrl.js
+++ controllers/products/productsCtrl.js
@@ -2,7 +2,18 @@
 
 const appRoot = process.cwd();
 
-const model = require(appRoot + "/models/ProductsModel");
+const { getSingleProduct, getAllProducts } = require(appRoot + "/models/ProductsModel");
 
+module.exports.getAllProducts = (req, res, next) =>
+  getAllProducts()
+    .then(products => {
+      res.status(200).json(products);
+    })
+    .catch(err => next(err));
 
-module.exports = {  };
+module.exports.getAllProducts = (req, res, next) =>
+  getSingleProduct(req.params.id)
+    .then(product => {
+      res.status(200).json(product);
+    })
+    .catch(err => next(err));
