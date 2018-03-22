208dcfc20b04e8d84c30a2b869049c16c9fa5c67
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 13:21:58 -0500

    add training_program pk

--- data/faker/employees/trainingProgramFaker.js
+++ data/faker/employees/trainingProgramFaker.js
@@ -6,12 +6,14 @@ const randomInt = require("../randomInt");
 module.exports.generateTrainingPrograms = (limit) => {
   let trainingPrograms = [];
   for (let i=0; i<limit; i++) {
+    let training_program_id = i;
     let name = faker.fake("{{name.jobArea}} 101");
     let start_date = faker.date.past(5, new Date());
     let end_date = faker.date.between(start_date, new Date());
     // max_capacity = random number between 10 and 20
     let max_capacity = randomInt(10) + 10;
     trainingPrograms.push({
+      training_program_id,
       name,
       start_date,
       end_date,
