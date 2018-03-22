4cb9cd7203a77e014e25534c2dd0d2498eed12d8
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 09:52:02 -0500

    used randomInt in employeeTrainingProgram

--- data/faker/employees/employeeTrainingProgramFaker.js
+++ data/faker/employees/employeeTrainingProgramFaker.js
@@ -1,12 +1,13 @@
 "use strict";
 
 const faker = require("faker");
+const randomInt = require("../randomInt");
 
 module.exports.generateEmployeeTrainingProgramRelationships = (employeeCount, trainingProgramCount) => {
   let employeeTrainingProgramRelationships = [];
   for (let i=0; i<employeeCount; i++) {
-    let employee_id = Math.floor(Math.random()*employeeCount) + 1;
-    let training_program_id = Math.floor(Math.random()*trainingProgramCount) + 1;
+    let employee_id = randomInt(employeeCount);
+    let training_program_id = randomInt(trainingProgramCount);
     employeeTrainingProgramRelationships.push({
       employee_id,
       training_program_id
