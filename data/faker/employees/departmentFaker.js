70929d32bcc62939f512730852541218ec87a7bf
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 09:55:35 -0500

    added expense_budget

--- data/faker/employees/departmentFaker.js
+++ data/faker/employees/departmentFaker.js
@@ -8,10 +8,12 @@ module.exports.generateDepartments = (limit, employeeCount) => {
   for (let i=0; i<limit; i++) {
     let department_id = i;
     let supervisor_id = randomInt(employeeCount);
+    let expense_budget = faker.random.number({min:5, max:10})*1000;
     let name = faker.commerce.department();
     depts.push({
       department_id,
       supervisor_id,
+      expense_budget,
       name
     });
   }
