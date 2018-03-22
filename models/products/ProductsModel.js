dd2e7b7f8ba85d49bc56367d6221ad9f6a2e9c1c
Tim A. Conner <timaconner1@gmail.com>
2018-03-22 13:51:18 -0500

    Add getAllProducts function.

--- models/products/ProductsModel.js
+++ models/products/ProductsModel.js
@@ -1,6 +1,23 @@
-"use strict";
-
+'use strict';
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/api-sprint.sqlite');
 
-module.exports = {};
+const getAllProducts = () => new Promise((resolve, reject) => 
+  db.all(
+    `SELECT *
+    FROM products`,
+    (err, products) => err ? reject(err) : resolve(products)
+));
+
+const putProduct = () => {};
+const patchProduct = () => {};
+const deleteProduct = () => {};
+const postProduct = () => {};
+
+module.exports = {
+  getAllProducts,
+  putProduct,
+  patchProduct,
+  deleteProduct,
+  postProduct
+};
