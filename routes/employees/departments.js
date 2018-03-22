5e7319a8dc39d5ff21fe27037229b19b909a3e93
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-03-22 14:49:28 -0500

    addresses issue #54

--- routes/employees/departments.js
+++ routes/employees/departments.js
@@ -5,9 +5,11 @@ const appRoot = process.cwd();
 const { Router } = require('express');
 const departmentsRouter = Router();
 
-const controller = require(appRoot + '/controllers/employees/departmentsCtrl');
+const { getAllDepartments, getSingleDepartment } = require(appRoot + '/controllers/employees/departmentsCtrl');
 
-//TODO: routes & their corresponding controllers are enumerated here
-departmentsRouter.get('/', controller);
+departmentsRouter.get('/departments', getAllDepartments);
+departmentsRouter.get('/departments/:id', getSingleDepartment);
 
 module.exports = departmentsRouter;
+
+
