ab934c750db19c9d0d119707db4c77d582502546
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-03-22 14:02:49 -0500

    added departments model

--- controllers/employees/departmentsCtrl.js
+++ controllers/employees/departmentsCtrl.js
@@ -1,8 +1,27 @@
 "use strict";
 
 const appRoot = process.cwd();
+const { getAllDepartments, getSingleDepartment } = require(appRoot + "/models/employees/DepartmentsModel");
 
-const model = require(appRoot + "/models/employees/DepartmentsModel");
+module.exports.getAllDirectors = (req, res, next) => {
+  getAll()
+    .then(directors => {
+      res.status(200).json(directors);
+    })
+    .catch(error => {
+      next(error);
+    })
+}
+
+module.exports.getOneDirector = (req, res, next) => {
+  getOneDirector(req.params.id)
+    .then(director => {
+      res.status(200).json(director);
+    })
+    .catch(error => {
+      next(error);
+    })
+}
 
 
 module.exports = {  };
