01c50c04e4a7c552bbfe7d565ee5b440206e86f3
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 09:22:35 -0500

    create order table following joe's create table files

--- db/products/makeOrderTable.js
+++ db/products/makeOrderTable.js
@@ -2,7 +2,23 @@
 
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("api-sprint.sqlite");
+const { generateSqlTable } = require("../sqlRunTemplate");
+const orders = require("../../data/json/orders.json");
 
 module.exports = () => {
-  // function that creates order table
+  generateSqlTable(
+    {
+      tableName: `orders`,
+      columns: 
+        `order_id INTEGER PRIMARY KEY,
+        customer_id INTEGER,
+        payment_option_id INTEGER`,
+      dataToIterateOver: orders,
+      valuesToInsert: [
+        null,
+        `order_id`,
+        `payment_option_id`
+      ]
+    }
+  )
 }
