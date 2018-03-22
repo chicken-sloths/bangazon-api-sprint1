4e7937ba86d3f91306556a874318d4e2b44d4c17
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 13:25:24 -0500

    merge master

--- db/employees/makeEmployeeTrainingTable.js
+++ db/employees/makeEmployeeTrainingTable.js
@@ -8,16 +8,16 @@ module.exports = () =>
     {
       tableName: 'Employee_Training_Programs',
       columns:
-	`employee_training_program_id INTEGER PRIMARY KEY,
-	training_program_id INT,
-	employee_id INT,
-	FOREIGN KEY (training_program_id) REFERENCES Training_Programs(training_program_id)
-	FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)`,
+        `employee_training_program_id INTEGER PRIMARY KEY,
+        training_program_id INT,
+        employee_id INT,
+        FOREIGN KEY (training_program_id) REFERENCES Training_Programs(training_program_id)
+        FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)`,
       dataToIterateOver: employeeTrainingProgram,
       valuesToInsert: [
-	null,
-	'training_program_id',
-	'employee_id']
+        null,
+        'training_program_id',
+        'employee_id']
     }
   );
 
