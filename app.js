97f8694be56aab3857a44cbdc481cc6c4ee1e269
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-22 14:31:48 -0500

    route, controller

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-// const routes = require('./routes/index.js'); 
+const routes = require('./routes/index.js'); 
+
+app.use(routes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
