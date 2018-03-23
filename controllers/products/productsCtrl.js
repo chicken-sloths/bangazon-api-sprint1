b2863387647d47f8355bbeb79c9b7d79b8259dbb
Tim A. Conner <timaconner1@gmail.com>
2018-03-23 14:27:52 -0500

    Add postProduct.

--- controllers/products/productsCtrl.js
+++ controllers/products/productsCtrl.js
@@ -1,17 +1,27 @@
 "use strict";
 const appRoot = process.cwd();
-const { getSingleProduct, getAllProducts } = require(appRoot + "/models/products/ProductsModel");
+const { getSingleProduct, getAllProducts, postProduct } = require(appRoot + "/models/products/ProductsModel");
 
-module.exports.getAllProducts = (req, res, next) =>
+module.exports.getAllProducts = (req, res, next) => {
   getAllProducts()
     .then(products => {
       res.status(200).json(products);
     })
     .catch(err => next(err));
+};
 
-module.exports.getSingleProduct = (req, res, next) =>
+module.exports.getSingleProduct = (req, res, next) => {
   getSingleProduct(req.params.id)
     .then(product => {
       res.status(200).json(product);
     })
     .catch(err => next(err));
+};
+
+module.exports.postProduct = (req, res, next) => {
+  postProduct(req.body)
+    .then(productId => {
+      res.status(200).json(productId);
+    })
+    .catch(err => next(err));
+};
--- models/products/ProductsModel.js
+++ models/products/ProductsModel.js
@@ -2,31 +2,46 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/api-sprint.sqlite');
 
-const getSingleProduct = id => new Promise((resolve, reject) => 
+const getSingleProduct = id => new Promise((resolve, reject) =>
   db.get(
     `SELECT *
     FROM Products
     WHERE product_id = ${id}`,
     (err, product) => err ? reject(err) : resolve(product)
-));
+  ));
 
-const getAllProducts = () => new Promise((resolve, reject) => 
+const getAllProducts = () => new Promise((resolve, reject) =>
   db.all(
     `SELECT *
     FROM Products`,
     (err, products) => err ? reject(err) : resolve(products)
-));
+  ));
 
-const putProduct = () => {};
-const patchProduct = () => {};
-const deleteProduct = () => {};
-const postProduct = () => {};
+const putProduct = () => { };
+
+const deleteProduct = () => { };
+
+const postProduct = ({ price, title, description, product_type_id, creator_id }) =>
+  new Promise((resolve, reject) => {
+    db.run(`INSERT INTO Products (
+      price,
+      title,
+      description,
+      product_type_id,
+      creator_id
+    ) VALUES (
+      '${price}',
+      '${title}',
+      '${description}',
+      '${product_type_id}',
+      '${creator_id}'
+    )`, err => err ? reject(err) : resolve(this.lastID));
+  });
 
 module.exports = {
   getSingleProduct,
   getAllProducts,
   putProduct,
-  patchProduct,
   deleteProduct,
   postProduct
 };
