ccc6e96abc986d77569af9553bf70b86a5d1828b
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-23 14:03:48 -0500

    pos

--- controllers/employees/employeesCtrl.js
+++ controllers/employees/employeesCtrl.js
@@ -2,9 +2,9 @@
 
 const appRoot = process.cwd();
 
-const { 
-  getAllEmployees, 
-  getSingleEmployee 
+const {
+  getAllEmployees,
+  getSingleEmployee
 } = require(appRoot + "/models/employees/EmployeesModel");
 
 module.exports.getAllEmployees = (req, res, next) =>
@@ -15,4 +15,11 @@ module.exports.getAllEmployees = (req, res, next) =>
 module.exports.getSingleEmployee = (req, res, next) =>
   getSingleEmployee(req.params.id)
   .then(emp => res.status(200).json(emp))
-  .catch(err => next(err)); 
+  .catch(err => next(err));
+
+module.exports.postEmployee = (req, res, next) => {
+  let { first_name = false, last_name = , department_id } = req.body;
+  postEmployee(req.body)
+  .then(() => res.status(201))
+  .catch(err => next(err))
+};
