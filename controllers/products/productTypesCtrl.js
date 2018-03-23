ccc4a3a1557a8298a785f619bdb2a7726a0ea3fd
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-23 17:20:04 -0500

    Enables PUT & POST request for Product_Types, according to #103

--- controllers/products/productTypesCtrl.js
+++ controllers/products/productTypesCtrl.js
@@ -4,7 +4,8 @@ const appRoot = process.cwd();
 
 const {
   getAllProductTypes,
-  getSingleProductType
+  getSingleProductType,
+  updateProductTypesTable
 } = require(appRoot + "/models/products/ProductTypesModel");
 
 module.exports.getAllProductTypes = (req, res, next) =>
@@ -16,3 +17,12 @@ module.exports.getSingleProductType = (req, res, next) =>
   getSingleProductType(req.params.id)
   .then(prodType => res.status(200).json(prodType))
   .catch(err => next(err));
+
+module.exports.updateProductTypesTable = (req, res, next) => {
+  let { title } = req.body,
+      { id = null } = req.params;
+  updateProductTypesTable(id, title)
+  .then(id => res.status(200).json(id))
+  .catch(err => next(err));
+};
+
