6b684e89af5b5a26a94408d873ef177bbbf938b8
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-23 13:18:39 -0500

    Fixes typo in require statement

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
-const bodyParser = reqiure('body-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index.js');
 
 // Middleware stack
