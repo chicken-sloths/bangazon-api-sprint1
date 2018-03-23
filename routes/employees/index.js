8e7b9fa622ca61e27608cafdd95eb999270166f6
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-23 14:06:22 -0500

    uncommented out routes

--- routes/employees/index.js
+++ routes/employees/index.js
@@ -5,9 +5,9 @@ const employeesIndexRouter = Router();
 
 employeesIndexRouter.use(require('./departments'));
 employeesIndexRouter.use('/employees', require('./employees'));
-// employeesIndexRouter.use(require('./employeeComputers'));
+employeesIndexRouter.use(require('./employeeComputers'));
 employeesIndexRouter.use("/computers", require('./computers'));
-// employeesIndexRouter.use(require('./trainingPrograms'));
-// employeesIndexRouter.use(require('./employeeTrainingPrograms'));
+employeesIndexRouter.use(require('./trainingPrograms'));
+employeesIndexRouter.use(require('./employeeTrainingPrograms'));
 
 module.exports = employeesIndexRouter;
