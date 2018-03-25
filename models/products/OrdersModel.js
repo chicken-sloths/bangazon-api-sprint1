f83a541bc35f475fac85b9799d941f348d7d1a08
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-03-25 07:48:24 -0500

    fixes issue #10

--- models/products/OrdersModel.js
+++ models/products/OrdersModel.js
@@ -3,6 +3,7 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/api-sprint.sqlite');
 
+// Gets all orders and a stringified list of associated products
 module.exports.getAllOrders = () => {
   return new Promise((resolve, reject) => {
     db.all(`SELECT Orders.*, group_concat(Products.title, ", ") AS Products
@@ -19,22 +20,35 @@ module.exports.getAllOrders = () => {
   })
 }
 
+// Makes two calles to the database, one to get the order info and another to get an array of the products associated with that order
 module.exports.getSingleOrder = id => {
+  let order = null;
   return new Promise((resolve, reject) => {
-    db.all(`SELECT Orders.*, Products.*
-      FROM Orders
-      INNER JOIN Product_Orders ON Orders.order_id = Product_Orders.order_id
-      INNER JOIN Products ON Product_Orders.product_id = Products.product_id
-      WHERE Orders.order_id =${id}`,
-      (err, order) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(order);
-      })
+    db.serialize(() => {
+      
+      //grabs Order data and declares it to the empty order variable 
+      db.get(`SELECT * FROM Orders WHERE order_id=${id}`,
+        (err, orderData) => {
+          if (err) reject(err);
+          order = orderData;
+        });
+
+        //grabs an array of all products associated with that order, adds it as a property on the order variable
+        db.all(`SELECT Products.*
+        FROM Orders
+        INNER JOIN Product_Orders ON Orders.order_id = Product_Orders.order_id
+        INNER JOIN Products ON Product_Orders.product_id = Products.product_id
+        WHERE Orders.order_id = ${ id}`,
+          (err, productData) => {
+            if (err) reject(err);
+            order.Products = productData
+            resolve(order);
+          });
+    })
   })
 }
 
+// Creates a new order
 module.exports.createOrder = ({ customer_id, payment_option_id }) => {
   return new Promise((resolve, reject) => {
     db.run(`INSERT INTO Orders(
@@ -51,6 +65,7 @@ module.exports.createOrder = ({ customer_id, payment_option_id }) => {
   })
 }
 
+// Updates information on an order by id
 module.exports.updateOrder = (id, { customer_id, payment_option_id }) => {
   return new Promise((resolve, reject) => {
     db.run(`REPLACE INTO Orders( 
@@ -69,6 +84,7 @@ module.exports.updateOrder = (id, { customer_id, payment_option_id }) => {
   });
 }
 
+// Deletes an order
 module.exports.deleteOrder = id => {
   return new Promise((resolve, reject) => {
     db.run(`DELETE FROM Orders WHERE order_id = ${id}`,
