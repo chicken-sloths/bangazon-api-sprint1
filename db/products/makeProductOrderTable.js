15726a759cb7d87d09aef25d6aff48fc7a62d945
Tim A. Conner <timaconner1@gmail.com>
2018-03-26 11:34:12 -0500

    Add ON DELETE CASCADE to order_id foreign key column.

--- db/products/makeProductOrderTable.js
+++ db/products/makeProductOrderTable.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { generateSqlTable } = require("../sqlRunTemplate");
-const productOrders = require("../../data/json/productOrders");
+const { generateSqlTable } = require(`../sqlRunTemplate`);
+const productOrders = require(`../../data/json/productOrders`);
 
 module.exports = () =>
   generateSqlTable(
@@ -12,7 +12,7 @@ module.exports = () =>
         product_id INT,
         order_id INT,
         FOREIGN KEY (product_id) REFERENCES Products(product_id),
-        FOREIGN KEY (order_id) REFERENCES Orders(order_id)`,
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE`,
       dataToIterateOver: productOrders,
       valuesToInsert: [
         `product_order_id`,
