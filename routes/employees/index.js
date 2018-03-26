feed0b41ddfb4f49611ddce013b59f2306485b52
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-26 11:05:32 -0500

    fixed routes so that the section prefix is in the index file of that folder, rather than in the individual routes files

--- routes/employees/index.js
+++ routes/employees/index.js
@@ -3,11 +3,11 @@
 const { Router } = require('express');
 const employeesIndexRouter = Router();
 
-employeesIndexRouter.use(require('./departments'));
+employeesIndexRouter.use('/departments', require('./departments'));
 employeesIndexRouter.use('/employees', require('./employees'));
-employeesIndexRouter.use(require('./employeeComputers'));
-employeesIndexRouter.use("/computers", require('./computers'));
-employeesIndexRouter.use("/training-programs", require('./trainingPrograms'));
-employeesIndexRouter.use(require('./employeeTrainingPrograms'));
+employeesIndexRouter.use('/computers', require('./computers'));
+employeesIndexRouter.use('/training-programs', require('./trainingPrograms'));
+// employeesIndexRouter.use('/employeeComputers', require('./employeeComputers'));
+// employeesIndexRouter.use('/employeeTrainingPrograms', require('./employeeTrainingPrograms'));
 
 module.exports = employeesIndexRouter;
--- routes/products/index.js
+++ routes/products/index.js
@@ -3,11 +3,11 @@
 const { Router } = require('express');
 const productsIndexRouter = Router();
 
+productsIndexRouter.use('/products', require('./products'));
 productsIndexRouter.use('/product-types', require('./productTypes'));
-productsIndexRouter.use(require('./products'));
-productsIndexRouter.use(require('./productOrders'));
-productsIndexRouter.use(require('./orders'));
-productsIndexRouter.use(require('./customers'));
-productsIndexRouter.use("/payment-options", require('./paymentOptions'));
+productsIndexRouter.use('/orders', require('./orders'));
+productsIndexRouter.use('/customers', require('./customers'));
+productsIndexRouter.use('/payment-options', require('./paymentOptions'));
+// productsIndexRouter.use('/productOrders', require('./productOrders'));
 
 module.exports = productsIndexRouter;
