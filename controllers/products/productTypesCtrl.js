2f1ab5bfdcb1e45a4dfce19bf2899ce00eae2ffd
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-26 13:24:02 -0500

    productTypesCtrl: Adds error handling & more 204 status codes for GET funcs

--- controllers/products/productTypesCtrl.js
+++ controllers/products/productTypesCtrl.js
@@ -11,20 +11,30 @@ const {
 
 module.exports.getAllProductTypes = (req, res, next) =>
   getAllProductTypes()
-  .then(prodTypes => res.status(200).json(prodTypes))
+  .then(prodTypes =>
+    prodTypes >= 1 ? res.status(200).json(prodTypes) : res.status(204).send()
+  )
   .catch(err => next(err));
 
 module.exports.getSingleProductType = (req, res, next) =>
   getSingleProductType(req.params.id)
-  .then(prodType => res.status(200).json(prodType))
+  .then(prodType =>
+    prodType >= 1 ? res.status(200).json(prodType) : res.status(204).send()
+  )
   .catch(err => next(err));
 
 module.exports.updateProductTypesTable = (req, res, next) => {
   let { title } = req.body,
       { id = null } = req.params;
-  updateProductTypesTable(id, title)
-  .then(id => res.status(200).json(id))
-  .catch(err => next(err));
+  if (title) {
+    updateProductTypesTable(id, title)
+    .then(id => res.status(200).json(id))
+    .catch(err => next(err));
+  } else {
+    const err = new Error("Please include: title")
+    err.status = 400;
+    return err;
+  }
 };
 
 module.exports.deleteProductType = (req, res, next) =>
