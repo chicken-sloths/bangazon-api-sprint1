15604a4442e37a7aeebb1159185473c7673719a1
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-26 13:23:50 -0500

    paymentOptionsCtrl: Adds error handling & more 204 status codes for GET funcs

--- controllers/products/paymentOptionsCtrl.js
+++ controllers/products/paymentOptionsCtrl.js
@@ -5,28 +5,18 @@ const options = require("../../models/products/PaymentOptionsModel");
 // get single payment option by id
 module.exports.getSinglePaymentOption = (req, res, next) => {
   options.getSinglePaymentOption(req.params.id)
-    .then(data => {
-      if (data.length >= 1) {
-        res.status(200).json(data);
-      } else {
-        let err = new Error("Payment Option not found.");
-        next(err);
-      }
-    })
+    .then(data =>
+      data.length >= 1 ? res.status(200).json(data) : res.status(204).send()
+    )
     .catch(err => next(err));
 };
 
 // get all payment options
 module.exports.getAllPaymentOptions = (req, res, next) => {
   options.getAllPaymentOptions()
-    .then(data => {
-      if (data.length >= 1) {
-        res.status(200).json(data);
-      } else {
-        let err = new Error("No Payment Options found.");
-        next(err);
-      }
-    })
+    .then(data =>
+      data.length >= 1 ? res.status(200).json(data) : res.status(204).send()
+    )
     .catch(err => next(err));
 };
 
@@ -40,14 +30,17 @@ module.exports.createNewPaymentOption = (req, res, next) => {
       })
       .catch(err => next(err));
   } else {
-    let err = new Error("Please include a `type`, `account_number`, and `customer_id`.");
+    const err = new Error(
+      "Please include: type, account_number, and customer_id."
+    );
+    err.status = 400;
     next(err);
   }
 };
 
 // update one payment option by id
 module.exports.updatePaymentOption = (req, res, next) => {
-  let { type, account_number, customer_id } = req.body;
+  const { type, account_number, customer_id } = req.body;
   if (type && account_number && customer_id) {
     options.updatePaymentOption(req.params.id, req.body)
       .then(data => {
@@ -55,7 +48,10 @@ module.exports.updatePaymentOption = (req, res, next) => {
       })
       .catch(err => next(err));
   } else {
-    let err = new Error("Please include a `type`, `account_number`, and `customer_id`.");
+    const err = new Error(
+      "Please include: type, account_number, and customer_id."
+    );
+    err.status = 400;
     next(err);
   }
 };
