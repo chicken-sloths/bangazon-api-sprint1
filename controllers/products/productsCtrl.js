6d301f7a224c0f5fb5420c6d3cfb65b4d6d8439a
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-26 13:24:10 -0500

    productsCtrl: Adds error handling & more 204 status codes for GET funcs

--- controllers/products/productsCtrl.js
+++ controllers/products/productsCtrl.js
@@ -1,29 +1,44 @@
 "use strict";
 const appRoot = process.cwd();
-const { getSingleProduct, getAllProducts, postProduct, putProduct, deleteProduct } = require(appRoot + "/models/products/ProductsModel");
+const {
+  getSingleProduct,
+  getAllProducts,
+  postProduct,
+  putProduct,
+  deleteProduct
+} = require(appRoot + "/models/products/ProductsModel");
 
 module.exports.getAllProducts = (req, res, next) => {
   getAllProducts()
-    .then(products => {
-      res.status(200).json(products);
-    })
+    .then(products =>
+      products >= 1 ? res.status(200).json(products) : res.status(204).send()
+    )
     .catch(err => next(err));
 };
 
 module.exports.getSingleProduct = (req, res, next) => {
   getSingleProduct(req.params.id)
-    .then(product => {
-      res.status(200).json(product);
-    })
+    .then(product =>
+      product >= 1 ? res.status(200).json(product) : res.status(204).send()
+    )
     .catch(err => next(err));
 };
 
 module.exports.postProduct = (req, res, next) => {
-  postProduct(req.body)
+  const { price, title, description, product_type_id, creator_id } = req.body;
+  if (price && title && description && product_type_id && creator_id ) {
+    postProduct(req.body)
     .then(productId => {
       res.status(200).json(productId);
     })
     .catch(err => next(err));
+  } else {
+    const err = new Error(
+      "Please include: price, title, description, product_type_id and creator_id"
+    );
+    err.status = 400;
+    return err;
+  }
 };
 
 module.exports.deleteProduct = (req, res, next) => {
@@ -32,7 +47,10 @@ module.exports.deleteProduct = (req, res, next) => {
       if(successfulDelete){
         res.status(200).json(successfulDelete);
       } else {
-        const err = new Error('The product could not be deleted.  It may not exist.');
+        const err = new Error(
+          "The product could not be deleted. It may not exist."
+        );
+        err.status = 400;
         next(err);
       }
     })
@@ -49,7 +67,10 @@ module.exports.putProduct = (req, res, next) => {
       })
       .catch(err => next(err));
   } else {
-    const err = new Error('Please supply a `price`, `title`, `description`, `product_type_id`, and `creator_id.');
+    const err = new Error(
+      "Please include: price, title, description, product_type_id and creator_id"
+    );
+    err.status = 400;
     next(err);
   }
 };
