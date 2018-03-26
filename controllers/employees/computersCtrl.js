1e27eda186fd0033f25e9ac89cda6ece6fc9e1a9
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-26 13:21:57 -0500

    computersCtrl: Adds error handling & more 204 status codes for GET funcs

--- controllers/employees/computersCtrl.js
+++ controllers/employees/computersCtrl.js
@@ -5,28 +5,18 @@ const computers = require("../../models/employees/ComputersModel");
 // gets one computer by id; throws error if none
 module.exports.getComputerById = (req, res, next) => {
   computers.getSingleComputer(req.params.id)
-    .then(data => {
-      if (data.length >= 1) {
-        res.status(200).json(data);
-      } else {
-        let err = new Error("Computer not found.");
-        next(err);
-      }
-    })
+    .then(data =>
+      data.length >= 1 ? res.status(200).json(data) : res.status(204).json()
+    )
     .catch(err => next(err));
 };
 
 // gets all computers; throws an error if none
 module.exports.getAllComputers = (req, res, next) => {
   computers.getAllComputers()
-    .then(data => {
-      if (data.length >= 1) {
-        res.status(200).json(data);
-      } else {
-        let err = new Error("No computers found.");
-        next(err);
-      }
-    })
+    .then(data =>
+      data.length >= 1 ? res.status(200).json(data) : res.status(204).json()
+    )
     .catch(err => next(err));
 }
 
@@ -36,12 +26,13 @@ module.exports.createNewComputer = (req, res, next) => {
   if (mac_address && purchase_date && decommission_date) {
     // ready to catch a SyntaxError (bad JSON)
     computers.createComputer(req.body)
-      .then(data => {
-        res.status(200).json(data);
-      })
+      .then(data => res.status(200).json(data))
       .catch(err => next(err));
   } else {
-    let err = new Error("Please supply a `mac_address`, `purchase_date`, and `decommission_date`.");
+    const err = new Error(
+      "Please include: mac_address, purchase_date, and decommission_date."
+    );
+    err.status = 400;
     next(err);
   }
 };
@@ -49,9 +40,7 @@ module.exports.createNewComputer = (req, res, next) => {
 // delete computer by id
 module.exports.deleteComputer = (req, res, next) => {
   computers.deleteComputer(req.params.id)
-    .then(id => {
-      res.status(200).json(id);
-    })
+    .then(id => res.status(200).json(id))
     .catch(err => next(err));
 };
 
@@ -59,12 +48,13 @@ module.exports.updateComputer = (req, res, next) => {
   let { mac_address, purchase_date, decommission_date } = req.body;
   if (mac_address && purchase_date && decommission_date) {
     computers.updateComputer(req.params.id, req.body)
-      .then(data => {
-        res.status(200).json(data);
-      })
+      .then(data => res.status(200).json(data))
       .catch(err => next(err));
   } else {
-    let err = new Error("Please supply a `mac_address`, `purchase_date`, and `decommission_date`.");
+    const err = new Error(
+      "Please include: mac_address, purchase_date, and decommission_date."
+    );
+    err.status = 400;
     next(err);
   }
 };
