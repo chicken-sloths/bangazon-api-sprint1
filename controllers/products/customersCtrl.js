841f392501292e0dc26d9436a763179a206afb04
David Lars Ketch <davidlarsketch@gmail.com>
2018-03-26 13:23:18 -0500

    customersCtrl: Adds error handling & more 204 status codes for GET funcs

--- controllers/products/customersCtrl.js
+++ controllers/products/customersCtrl.js
@@ -4,63 +4,88 @@ const appRoot = process.cwd();
 const { getAll, getOne, getFrugalCustomers, postNew, updateOne } = require(appRoot + "/models/products/CustomersModel");
 
 const getAllCustomers = (req, res, next) =>{
-  // This checks for the exact query url that was listed 
+  // This checks for the exact query url that was listed
   // in the wiki page for customers in our repo
   if(req.query.active === "false"){
     getFrugalCustomers()
-  .then((customers)=>{
-    res.status(200).json(customers);
-  })
-  .catch(error=>{
-    next(error);
+  .then(customers =>
+    customers >= 1 ? res.status(200).json(customers) : res.status(204).send()
+  )
+  .catch(err=>{
+    next(err);
   });
   }else{
     getAll()
-    .then((customers)=>{
-      res.status(200).json(customers);
-    })
-    .catch(error=>{
-      next(error);
-    });
+    .then(customers =>
+      customers >= 1 ? res.status(200).json(customers) : res.status(204).send()
+    )
+    .catch(err => next(err));
   }
 };
 
 const getOneCustomer = (req, res, next) =>{
   getOne(req.params.id)
-  .then((customer)=>{
-    res.status(200).json(customer);
-  })
-  .catch(error=>{
-    next(error);
-  });
+  .then(customer =>
+    customer >= 1 ? res.status(200).json(customer) : res.status(204).send()
+  )
+  .catch(err => next(err));
 };
 
 const postNewCustomer = (req, res, next) =>{
-  postNew(req.body)
-  .then(newData=>{
-    res.status(200).json(newData);
-  })
-  .catch(err=>{
-    console.log('err in ctrl:',err);
+  const {
+    first_name,
+    last_name,
+    account_creation_date,
+    street_address,
+    city,
+    state,
+    postal_code,
+    phone_number
+  } = req.body;
+
+  if (first_name && last_name && account_creation_date && street_address && city && state && postal_code && phone_number) {
+    postNew(req.body)
+    .then(newData=>{
+      res.status(200).json(newData);
+    })
+    .catch(err=>{
+      next(err);
+    });
+  } else {
+    const err = new Error(
+      "Please include: first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number"
+    );
+    err.status = 400;
     next(err);
-  });
+  }
 };
 
 const updateCustomer = (req, res, next) => {
-  let { first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number } = req.body;
-  if (first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number) {
+  const {
+    first_name,
+    last_name,
+    account_creation_date,
+    street_address,
+    city,
+    state,
+    postal_code,
+    phone_number
+  } = req.body;
+
+  if (first_name && last_name && account_creation_date && street_address && city && state && postal_code && phone_number) {
     updateOne(req.params.id, req.body)
-      .then(data => {
-        res.status(200).json(data);
-      })
+      .then(data => res.status(200).json(data))
       .catch(err => next(err));
   } else {
-    let err = new Error("Please make sure the object has a value for each of these properties: first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number");
+    const err = new Error(
+      "Please include: first_name, last_name, account_creation_date, street_address, city, state, postal_code, phone_number"
+    );
+    err.status = 400;
     next(err);
   }
-}; 
+};
 
-module.exports = { 
+module.exports = {
   getAllCustomers,
   getOneCustomer,
   postNewCustomer,
