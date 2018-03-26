b57cc0e93cb73b785743f3d8a118455e84f104c4
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-26 11:34:13 -0500

    removed the two join table route prefix lines from the employees routes index file

--- routes/employees/index.js
+++ routes/employees/index.js
@@ -7,7 +7,5 @@ employeesIndexRouter.use('/departments', require('./departments'));
 employeesIndexRouter.use('/employees', require('./employees'));
 employeesIndexRouter.use('/computers', require('./computers'));
 employeesIndexRouter.use('/training-programs', require('./trainingPrograms'));
-// employeesIndexRouter.use('/employeeComputers', require('./employeeComputers'));
-// employeesIndexRouter.use('/employeeTrainingPrograms', require('./employeeTrainingPrograms'));
 
 module.exports = employeesIndexRouter;
